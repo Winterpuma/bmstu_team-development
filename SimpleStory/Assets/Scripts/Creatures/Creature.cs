3f154494efd2dc9e9fc1a989b1328ac8186012db
Cool Doge <32227002+UselessSaint@users.noreply.github.com>
2021-04-05 12:37:00 +0300

    Внес правки #14

--- SimpleStory/Assets/Scripts/Creatures/Creature.cs
+++ SimpleStory/Assets/Scripts/Creatures/Creature.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
-    [SerializeField]
-    private float _movementSpeed;
-    private const float _maxMovementSpeed = 7.0f;
-    private const float _minMovementSpeed = 0.0f;
-
-    [SerializeField]
-    private float _accelerationSpeed;
-    private const float _defaultAcceleration = 0.5f;
-
-    [SerializeField]
-    private float _decelerationSpeed;
-    private const float _defaultDecelerationSpeed = 0.75f;
-
-    [SerializeField]
-    private Vector3 _movementDirection;
-
     enum CreatureAction { idle, moovingRight, moovingLeft, stopMooving };
     [SerializeField]
     private CreatureAction _creatrureAction;
 
+    private const float _maxMovementSpeed = 7.0f;
+    private const float _minMovementSpeed = 0.0f;
+    private const float _defaultAcceleration = 0.5f;
+    private const float _defaultDecelerationSpeed = 0.75f;
+
+    [SerializeField]
+    private float _movementSpeed;
+    [SerializeField]
+    private float _accelerationSpeed;
+    [SerializeField]
+    private float _decelerationSpeed;
+    [SerializeField]
+    private Vector3 _movementDirection;
+    [SerializeField]
     private bool _isGrounded;
+
+    // Этот метод вызывается перед Start(), мб стоит перенести туда, точно не уверен.
 
     private void Awake()
     {
@@ -36,89 +36,43 @@ public class Creature : MonoBehaviour
         _isGrounded = false;
     }
 
-// Это я сделал для тестов, по хорошему это кусок кода персонажа
-// Так как это будет полезно, я это тут оставляю
+    // Методы для передвижения, нужно будет еще добавить метод Jump()
 
-/*
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.D)) 
-        {
-            _creatrureAction = CreatureAction.moovingRight;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            _creatrureAction = CreatureAction.moovingLeft;
-        }
-        else if (_movementSpeed > 0.0f)
-        {
-            _creatrureAction = CreatureAction.stopMooving;
-        }
-        else
-        {
-            _creatrureAction = CreatureAction.idle;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (_creatrureAction == CreatureAction.moovingRight)
-        {
-            recalculateSpeed(true);
-            moveRight();
-        }
-        else if (_creatrureAction == CreatureAction.moovingLeft)
-        {
-            recalculateSpeed(true);
-            moveLeft();
-        }
-        else if (_creatrureAction == CreatureAction.stopMooving)
-        {
-            recalculateSpeed(false);
-            postMoovement();
-        }
-    }
-*/
     private void moveRight() 
     {
         _movementDirection = Vector3.right;
-        transform.position += (_movementDirection*_movementSpeed*Time.fixedDeltaTime);
+        transform.position += _movementDirection * _movementSpeed * Time.fixedDeltaTime;
     }
 
     private void moveLeft() 
     {
         _movementDirection = Vector3.left;
-        transform.position += (_movementDirection*_movementSpeed*Time.fixedDeltaTime);
+        transform.position += _movementDirection * _movementSpeed * Time.fixedDeltaTime;
     }
 
     private void postMoovement()
     {
-        transform.position += (_movementDirection*_movementSpeed*Time.fixedDeltaTime);
+        transform.position += _movementDirection * _movementSpeed * Time.fixedDeltaTime;
     }
+
+    // Пересчет скорости, 
+    // Нужно будет еще добавить метод IsGrounded() для проверки находится ли объект на земле
 
     private void recalculateSpeed(bool isMooving)
     {
         if (isMooving)
         {
             if (_movementSpeed < _maxMovementSpeed)
-            {
                 _movementSpeed += _accelerationSpeed;
-            }
             else if (_movementSpeed > _maxMovementSpeed)
-            {
                 _movementSpeed = _maxMovementSpeed;
-            }
         }
         else
         {
             if (_movementSpeed > _minMovementSpeed)
-            {
                 _movementSpeed -= _decelerationSpeed;
-            }
             else if (_movementSpeed < _minMovementSpeed)
-            {
                 _movementSpeed = _minMovementSpeed;
-            }
         }
     }
 }
