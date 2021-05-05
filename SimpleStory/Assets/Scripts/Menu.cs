ac8aa0decc77a9099e3a6ef4907b294f4ed40919
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2021-05-05 13:44:16 +0300

    Remove unused using

--- SimpleStory/Assets/Scripts/Menu.cs
+++ SimpleStory/Assets/Scripts/Menu.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 /// <summary>
 /// Скрипт меню 
