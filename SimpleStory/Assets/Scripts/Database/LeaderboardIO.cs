ce2490d81007aa5d3827d2521e4dca794e89b903
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2022-09-26 02:26:08 +0300

    Add leaderboard IO

---
+++ SimpleStory/Assets/Scripts/Database/LeaderboardIO.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Assets.Scripts.Database
+{
+	internal class LeaderboardIO
+	{
+		private static string _filename = "leaderboard.db";
+
+		/// <summary>
+		/// Записывает в конец файла строчку лидерборда
+		/// </summary>
+		public static void Write(LeaderboardRow leaderboard)
+		{
+			using (var stream = File.Open(_filename, FileMode.Append))
+			{
+				using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+				{
+					writer.Write(leaderboard.PlayerName);
+					writer.Write(leaderboard.MapId);
+					writer.Write(leaderboard.WalkthroughTime);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Считывает лидерборд из файла
+		/// </summary>
+		public static List<LeaderboardRow> GetLeaderboard()
+		{
+			List<LeaderboardRow> list = new List<LeaderboardRow>();
+
+
+			if (File.Exists(_filename))
+			{
+				using (var stream = File.Open(_filename, FileMode.Open))
+				{
+					using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+					{
+						while (reader.BaseStream.Position != reader.BaseStream.Length)
+						{
+							LeaderboardRow row = new LeaderboardRow();
+
+							row.PlayerName = reader.ReadString();
+							row.MapId = reader.ReadInt32();
+							row.WalkthroughTime = reader.ReadDouble();
+
+							list.Add(row);
+						}
+					}
+				}
+			}
+
+			return list;
+		}
+	}
+}
---
+++ SimpleStory/Assets/Scripts/Database/LeaderboardRow.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Database
+{
+	internal class LeaderboardRow
+	{
+		public string PlayerName { get; set; }
+		public int MapId { get; set; }
+		public double WalkthroughTime { get; set; }
+
+	}
+}
